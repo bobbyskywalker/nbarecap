7cb44bb6392d6b6047affbc0ac757ed69a77ae0b
bobbyskywalker <aleksander.garbacz@gmail.com>
2026-01-23 18:55:04 +0100

    12-clients-refactor: client fetch logic improvements in progress

--- pkg/nba_api/clients/boxScoreTraditionalV3.go
+++ pkg/nba_api/clients/boxScoreTraditionalV3.go
@@ -1,24 +1,20 @@
 package clients
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
 const boxScoreV3UrlFormat = "boxscoretraditionalv3?GameID=%s&StartPeriod=0&EndPeriod=0&StartRange=0&EndRange=0&RangeType=0"
 
-func (apiClient *NbaApiClient) FetchBoxScoreTraditionalV3Json(gameID string) (json.RawMessage, error) {
+func (apiClient *NbaApiClient) FetchBoxScoreTraditionalV3(gameID string) error {
 	url := apiClient.baseUrl + apiClient.statsSuffix + fmt.Sprintf(boxScoreV3UrlFormat, gameID)
 
-	ctx, cancel := context.WithTimeout(context.Background(), defaultTimeout)
-	defer cancel()
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	setCommonRequestHeaders(req)
-	return getCommonJsonResponse(apiClient, req)
+	err = sendCommonGetRequest(apiClient, url)
+	return err
 }
