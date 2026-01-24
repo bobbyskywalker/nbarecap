a9356dba30f1760294f2dcfb4a461cf5ac4a03ce
bobbyskywalker <aleksander.garbacz@gmail.com>
2026-01-24 14:43:17 +0100

    12-clients-refactor: reduntant request builder for box score client removed

--- pkg/nba_api/clients/boxScoreTraditionalV3.go
+++ pkg/nba_api/clients/boxScoreTraditionalV3.go
@@ -2,19 +2,12 @@ package clients
 
 import (
 	"fmt"
-	"net/http"
 )
 
 const boxScoreV3UrlFormat = "boxscoretraditionalv3?GameID=%s&StartPeriod=0&EndPeriod=0&StartRange=0&EndRange=0&RangeType=0"
 
 func (apiClient *NbaApiClient) FetchBoxScoreTraditionalV3(gameID string) error {
 	url := apiClient.baseUrl + apiClient.statsSuffix + fmt.Sprintf(boxScoreV3UrlFormat, gameID)
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		return err
-	}
-	setCommonRequestHeaders(req)
-	err = sendCommonGetRequest(apiClient, url)
+	err := sendCommonGetRequest(apiClient, url)
 	return err
 }
--- pkg/nba_api/clients/scoreboardV2.go
+++ pkg/nba_api/clients/scoreboardV2.go
@@ -8,7 +8,6 @@ const scoreBoardV2UrlFormat = "scoreboardv2?GameDate=%s&LeagueID=00&DayOffset=0"
 
 func (apiClient *NbaApiClient) FetchScoreBoardV2(date string) error {
 	url := apiClient.baseUrl + apiClient.statsSuffix + fmt.Sprintf(scoreBoardV2UrlFormat, date)
-
 	err := sendCommonGetRequest(apiClient, url)
 	return err
 }
