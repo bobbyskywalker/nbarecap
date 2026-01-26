846d80a695dc186141eff3d897ffb598bfc2d395
bobbyskywalker <159538968+bobbyskywalker@users.noreply.github.com>
2026-01-26 22:35:46 +0100

    17-ui-app-refactor: app state handlers moved to separate file to ensure maintainability and clarity (#19)

--- internal/ui/app.go
+++ internal/ui/app.go
@@ -85,10 +85,6 @@ func (m appModel) fetchBoxScoreCmd(gameID string) tea.Cmd {
 	}
 }
 
-func updateDate(date time.Time, dateDelta int) time.Time {
-	return date.AddDate(0, 0, dateDelta)
-}
-
 /* Tea program */
 
 func (m appModel) Init() tea.Cmd {
@@ -113,77 +109,10 @@ func (m appModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch m.state {
 
 	case games:
-		var cmd tea.Cmd
-		m.list, cmd = m.list.Update(msg)
-
-		switch msg := msg.(type) {
-		case baseGameInfoMsg:
-			if msg.err != nil {
-				m.err = msg.err
-				return m, nil
-			}
-			m.err = nil
-			m.list.SetItems(msg.items)
-			m.numGames = len(msg.items)
-			return m, nil
-
-		case tea.KeyMsg:
-			switch msg.String() {
-			case "left":
-				m.date = updateDate(m.date, -dayStep)
-				return m, m.buildBaseGameInfoList()
-
-			case "right":
-				m.date = updateDate(m.date, dayStep)
-				return m, m.buildBaseGameInfoList()
-
-			case "enter":
-				it, ok := m.list.SelectedItem().(gameInfoItem)
-				if !ok {
-					return m, cmd
-				}
-				m.choice = &it
-				m.showingAway = true
-				m.state = boxScore
-				m.err = nil
-				m.currentBoxScore = nil
-
-				return m, tea.Batch(cmd, m.fetchBoxScoreCmd(it.id))
-			}
-		}
-
-		return m, cmd
+		return updateGamesState(m, msg)
 
 	case boxScore:
-		var cmd tea.Cmd
-		m.boxTable, cmd = m.boxTable.Update(msg)
-
-		switch msg := msg.(type) {
-		case boxScoreMsg:
-			if msg.err != nil {
-				m.err = msg.err
-				return m, nil
-			}
-			m.err = nil
-			m.currentBoxScore = msg.score
-			m.boxTable = newBoxScoreTable(msg.score, m.showingAway)
-			return m, nil
-
-		case tea.KeyMsg:
-			switch msg.String() {
-			case "esc", "backspace":
-				m.state = games
-				return m, nil
-			case "left", "right":
-				m.showingAway = !m.showingAway
-				if m.currentBoxScore != nil {
-					m.boxTable = newBoxScoreTable(m.currentBoxScore, m.showingAway)
-				}
-				return m, nil
-			}
-		}
-
-		return m, cmd
+		return updateBoxScoreState(m, msg)
 
 	default:
 		panic("unknown state")
